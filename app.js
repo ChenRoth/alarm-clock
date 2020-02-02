9c35c9472f74eb2e567f0dcc094697385bb5e480
Chen Roth <rothchen@gmail.com>
2020-02-02 22:51:29 +0200

    add logic for setting the alarm

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('code is loaded!');
+function setAlarm(alarmTime) {
+    const now = new Date();
+    // reset the date parts so we can compare times
+    resetDatePart(alarmTime);
+    resetDatePart(now);
+
+    if (alarmTime < now) {
+        // time of the alarm is earlier than the current time,
+        // so we need to set the alarm for the next day
+        alarmTime.setDate(alarmTime.getDate() + 1);
+    }
+
+    // the remaining time for the alarm is the difference in milliseconds between the dates
+    const timeForAlarmInMs = alarmTime - now;
+
+    setTimeout(() => {
+        console.log('GOOD MORNING!');
+    }, timeForAlarmInMs);
+}
+
+/*
+ reset the dart part of a Date obj to 1-1-2000
+ using this function, it's easy to compare the time part of 2 Date objects
+*/
+function resetDatePart(date) {
+    date.setDate(1);
+    date.setMonth(0);
+    date.setFullYear(2000);
+}
